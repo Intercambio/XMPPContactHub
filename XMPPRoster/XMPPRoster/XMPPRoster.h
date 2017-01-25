fb9d68e82931aef06a2c19932f698af609c32842
Tobias Kräntzer <info@tobias-kraentzer.de>
2017-01-25 18:01:49 +0100

    Initial Commit

---
+++ XMPPRoster/XMPPRoster/XMPPRoster.h
@@ -0,0 +1,17 @@
+//
+//  XMPPRoster.h
+//  XMPPRoster
+//
+//  Created by Tobias Kraentzer on 25.01.17.
+//  Copyright © 2017 Tobias Kräntzer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XMPPRoster.
+FOUNDATION_EXPORT double XMPPRosterVersionNumber;
+
+//! Project version string for XMPPRoster.
+FOUNDATION_EXPORT const unsigned char XMPPRosterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPPRoster/PublicHeader.h>
